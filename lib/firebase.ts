14ab3f9cded9b0ee3d9bd32e5eff84c6c6bbcaf4
fatemeh <fatemehabbasi411@gmail.com>
2025-11-04 23:11:35 +0330

    fix : update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB95xyVzASXmiogXbo4hnn3SwkBEY-3CmA",
-  authDomain: "crypto-dashboard-e2a51.firebaseapp.com",
-  projectId: "crypto-dashboard-e2a51",
-  storageBucket: "crypto-dashboard-e2a51.firebasestorage.app",
-  messagingSenderId: "168801150947",
-  appId: "1:168801150947:web:7dd4b1a549363d7437a2d6",
-  measurementId: "G-2V343DDK6Z",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
